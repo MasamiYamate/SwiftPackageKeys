30f1f98b5682521fafacf4ee54a7ed9d311568af
Masami Yamate <m.yamate@mail.yamatte.net>
2023-02-11 02:51:50 +0900

    Add gen script

---
+++ BuildTools/env_gen.sh
@@ -0,0 +1,41 @@
+# Extension file name
+extensionName="SwiftPackageKeys+Extension.swift"
+
+# Get Pass
+packagePath=$1
+pluginWorkDirPath=$2
+
+derivedDataPath=`echo ${packagePath%/*/*/*}`
+infoPlistPath="$derivedDataPath/info.plist"
+appProjectPath=`/usr/libexec/PlistBuddy -c "print WorkspacePath" $infoPlistPath`
+appDirPath=`echo ${appProjectPath%/*}`
+
+dotEnvPath=${appDirPath}/.env
+
+function generateEnvironmentProperty() {
+    dotEnvLine=$1
+    keyValue=(`echo ${dotEnvLine//=/ }`)
+    key=${keyValue[1]}
+    value=${keyValue[2]}
+    
+    response="
+    public var ${key}: String {
+        return \"${value}\"
+    }
+    "
+    echo $response
+}
+
+extensionCodeValue="public extension SwiftPackageKeys {
+"
+
+cat $dotEnvPath | while read line
+do
+property=`generateEnvironmentProperty $line`
+extensionCodeValue=$extensionCodeValue$property
+done
+
+extensionCodeValue="${extensionCodeValue}
+}"
+
+echo $extensionCodeValue > "${pluginWorkDirPath}/${extensionName}"
---
+++ Makefile
@@ -0,0 +1,2 @@
+generate_keys:
+	zsh ${PACKAGE_DIR}/BuildTools/env_gen.sh ${PACKAGE_DIR} ${WORK_DIR}
