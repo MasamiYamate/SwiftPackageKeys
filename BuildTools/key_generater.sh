ad3e96721168a176e1458361ed20a9b4b24f5f27
Masami Yamate <m.yamate@mail.yamatte.net>
2023-02-12 01:58:28 +0900

    Shell bug fix

--- BuildTools/key_generater.sh
+++ BuildTools/key_generater.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Extension file name
 EXTENSION_NAME="SwiftPackageKeys+Extension.swift"
 
@@ -6,18 +5,20 @@ EXTENSION_NAME="SwiftPackageKeys+Extension.swift"
 PACKAGE_PATH=$1
 PLUGIN_WORK_DIR_PATH=$2
 
-generate_environment_property() {
-    local LINE_VALUE
-    local DOT_ENV_ITEM
-    local KEY
-    local VALUE
-    local RESPONSE
-    
+DERIVED_DATA_PATH=`echo ${PACKAGE_PATH%/*/*/*}`
+INFO_PLIST_PATH=${DERIVED_DATA_PATH}/info.plist
+
+APP_PROJECT_PATH=`/usr/libexec/PlistBuddy -c "print WorkspacePath" $INFO_PLIST_PATH`
+APP_DIR_PATH=`echo ${APP_PROJECT_PATH%/*}`
+
+DOT_ENV_PATH=${APP_DIR_PATH}/.env
+
+function generateEnvironmentProperty() {
     LINE_VALUE=$1
     DOT_ENV_ITEM=(`echo ${LINE_VALUE//=/ }`)
     RAW_KEY=${DOT_ENV_ITEM[0]}
-    VALUE=${DOT_ENV_ITEM[1]}
     CAMEL_CASE_KEY=`echo $RAW_KEY | tr "[:upper:]" "[:lower:]" | awk -F '_' '{ printf $1; for(i=2; i<=NF; i++) {printf toupper(substr($i,1,1)) substr($i,2)}} END {print ""}'`
+    VALUE=${DOT_ENV_ITEM[1]}
     RESPONSE="
     public static var ${CAMEL_CASE_KEY}: String {
         return \"${VALUE}\"
@@ -26,42 +27,16 @@ generate_environment_property() {
     echo $RESPONSE
 }
 
-generate_swift_package_keys_extension() {
-    local DOT_ENV_PATH
-    local extension_code_value
-    local PROPERTY
-
-    DOT_ENV_PATH=$1
-
-    extension_code_value="public extension SwiftPackageKeys {
+extension_code_value="public extension SwiftPackageKeys {
 "
-    cat $DOT_ENV_PATH | while read line
-    do
-        PROPERTY=`generate_environment_property $line`
-        echo $PROPERTY
-        extension_code_value=$extension_code_value$PROPERTY
-    done
-        extension_code_value="${extension_code_value}
+
+cat $DOT_ENV_PATH | while read line
+do
+PROPERTY=`generateEnvironmentProperty $line`
+extension_code_value=$extension_code_value$PROPERTY
+done
+
+extension_code_value="${extension_code_value}
 }"
-    echo $extension_code_value > "${PLUGIN_WORK_DIR_PATH}/${EXTENSION_NAME}"
-}
 
-fetch_dot_env_path() {
-    local DERIVED_DATA_PATH
-    local INFO_PLIST_PATH
-    local APP_PROJECT_PATHh
-    local APP_DIR_PATH
-    local DOT_ENV_PATH
-
-    DERIVED_DATA_PATH=`echo ${PACKAGE_PATH%/*/*/*}`
-    INFO_PLIST_PATH=${DERIVED_DATA_PATH}/info.plist
-
-    APP_PROJECT_PATH=`/usr/libexec/PlistBuddy -c "print WorkspacePath" $INFO_PLIST_PATH`
-    APP_DIR_PATH=`echo ${APP_PROJECT_PATH%/*}`
-
-    DOT_ENV_PATH=${APP_DIR_PATH}/.env
-    echo $DOT_ENV_PATH
-}
-
-DOT_ENV_PATH=`fetch_dot_env_path`
-generate_swift_package_keys_extension $DOT_ENV_PATH
+echo $extension_code_value > "${PLUGIN_WORK_DIR_PATH}/${EXTENSION_NAME}"
